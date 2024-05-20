cc8ee17ec54b017722eb2fefd5759c7787f894b5
JasmineQian <942812509@qq.com>
2024-05-20 13:37:55 +0800

    db settings changes

---
+++ src/main/java/com/hackathon/webapp/service/UserServiceImpl.java
@@ -0,0 +1,64 @@
+package com.hackathon.webapp.service;
+
+import com.hackathon.webapp.entity.User;
+import com.hackathon.webapp.security.SecurityUtils;
+import com.hackathon.webapp.dao.UserDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Validated
+@Service
+public class UserServiceImpl implements UserService {
+
+    private UserDAO userDAO;
+
+    @Autowired
+    public UserServiceImpl(UserDAO userDAO){
+        this.userDAO = userDAO;
+    }
+
+    @Override
+    @Transactional
+    public List<User> listAllUser() {
+        return userDAO.listAllUser();
+    }
+
+    @Override
+    @Transactional
+    public User findByEmail(String theEmail) {
+        return userDAO.findByEmail(theEmail);
+    }
+
+    @Override
+    @Transactional
+    public void createUser(User theUser) {
+        theUser.setCreatedTime(LocalDateTime.now().toString());
+        userDAO.createUser(theUser);
+    }
+
+    @Override
+    @Transactional
+    public void updateUser(User theUser) {
+        userDAO.updateUser(theUser);
+    }
+
+    @Override
+    @Transactional
+    public void deleteUser(String theUsername) {
+        userDAO.deleteUser(theUsername);
+    }
+
+    @Override
+    public String login(String username, String password) {
+        User customer = userDAO.findByEmail(username);
+        if(customer != null && SecurityUtils.match(password, customer.getPassword())){
+            return SecurityUtils.getAuthToken(customer);
+        }
+        return null;
+    }
+}
